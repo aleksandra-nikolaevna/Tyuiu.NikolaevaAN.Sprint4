070ce235ee9c0033ad96c2dc11c0688f603e1763
Tyuiu.NikolaevaAN@outlook.com <Tyuiu.NikolaevaAN@outlook.com>
2023-11-04 00:50:27 +0500

    Task2 V5

---
+++ Tyuiu.NikolaevaAN.Sprint4.Task2.V5.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.NikolaevaAN.Sprint4.Task2.V5.Lib;
+
+namespace Tyuiu.NikolaevaAN.Sprint4.Task2.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            int[] numsArray = { 3, 4, 5, 6, 7, 8, 9, 3, 4, 5, 6, 7, 8, 9 };
+            int res = ds.Calculate(numsArray);
+            int wait = 36864;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.NikolaevaAN.Sprint4.Task2.V5/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.NikolaevaAN.Sprint4.Task2.V5.Lib;
+
+namespace Tyuiu.NikolaevaAN.Sprint4.Task2.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнила: Николаева А. Н. | ПКТб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы                                                *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнила: Николаева Александра Николаевна | ПКТб-23-2                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая подсчитает произведение четных элементов    *");
+            Console.WriteLine("* массива.                                                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[] numsArray = new int[14];
+            for (int i = 0; i < 14; i++)
+            {
+                numsArray[i] = rnd.Next(3, 10);
+            }
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < 14; i++)
+            {
+                Console.Write(numsArray[i] + "\t");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Произведение чётных элементов массива = " + ds.Calculate(numsArray));
+            Console.ReadKey();
+        }
+    }
+}
